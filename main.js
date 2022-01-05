1814c7072eadeff8a5828d509f9f75efb8f3c7a9
Taylor Woods <temwoods@gmail.com>
2022-01-05 15:08:57 -0500

    btn done

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.log('js:loaded')
+
+const feedbtn = document.getElementById("feedbtn")
+console.log(feedbtn)
+feedbtn.addEventListener("click", feed)
+
+function feed() {
+    console.log("feedthepet")
+}
+
+const sleepbtn = document.getElementById("sleepbtn")
+console.log(sleepbtn)
+sleepbtn.addEventListener("click", sleep)
+
+function sleep() {
+    console.log("putpettobed")
+}
+
+const playbtn = document.getElementById("playbtn")
+console.log(playbtn)
+playbtn.addEventListener("click", play)
+
+function play() {
+    console.log("playwiththepet")
+}
