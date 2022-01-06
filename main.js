db5d4ab0225a5e6867a7681a407ec60e062b7e3d
Taylor Woods <temwoods@gmail.com>
2022-01-06 13:54:41 -0500

    timers setup

--- main.js
+++ main.js
@@ -1,25 +1,78 @@
 console.log('js:loaded')
 
+let hunger = 0
+let sleepiness = 0
+let boredom = 0
+
 const feedbtn = document.getElementById("feedbtn")
-console.log(feedbtn)
+//console.log(feedbtn)
 feedbtn.addEventListener("click", feed)
 
 function feed() {
     console.log("feedthepet")
+    let petHunger = hunger
+    
+    hunger = hunger-1
+    console.log(hunger)
+    hungervalue.innerText = hunger
 }
 
+
 const sleepbtn = document.getElementById("sleepbtn")
-console.log(sleepbtn)
+
 sleepbtn.addEventListener("click", sleep)
 
 function sleep() {
     console.log("putpettobed")
+    let petSleepiness = sleepiness
+    sleepiness = sleepiness-1
+    console.log(sleepiness)
 }
 
 const playbtn = document.getElementById("playbtn")
-console.log(playbtn)
+
 playbtn.addEventListener("click", play)
 
 function play() {
     console.log("playwiththepet")
+    let petBoredom = boredom
+    boredom = boredom-1
+    console.log(boredom)
 }
+
+const lightsbtn = document.getElementById("lightsbtn")
+lightsbtn.addEventListener("click", onoff)
+function onoff() {
+    console.log("turnlightsonoff")
+}
+
+const hungerNeeds = document.querySelector("#hunger-needs")
+
+const foodNeeds = document.querySelector("#sleep-needs")
+
+const playNeeds = document.querySelector("#play-needs")
+
+let gameTimer = setInterval( game , 2000)
+
+function game() {
+   
+    hunger = hunger+1
+    sleepiness = sleepiness+1
+    boredom = boredom+1
+    hungervalue.innerText = hunger
+    sleepinessvalue.innerText = sleepiness
+    boredomvalue.innerText = boredom
+    console.log("hungerlevel" , hunger , "slepinesslevel" , sleepiness , "boredom", boredom)
+    if (hunger == 10) {
+        alert("game over")
+        clearInterval(gameTimer)
+    }
+}
+
+let hungervalue = document.getElementById("hungervalue")
+let sleepinessvalue = document.getElementById("sleepinessvalue")
+let boredomvalue = document.getElementById("boredomvalue")
+
+
+
+    
